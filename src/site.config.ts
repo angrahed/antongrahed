4a44a084c41002eb28d0589c85e94c5995ee0362
Anton Grahed <86027498+anton-grahed@users.noreply.github.com>
2025-10-27 10:52:32 +0000

    new design, added stamps category

--- src/site.config.ts
+++ src/site.config.ts
@@ -48,6 +48,10 @@ export const menuLinks: { path: string; title: string }[] = [
 		path: "/film/",
 		title: "Film",
 	},
+	{
+		path: "/stamps/",
+		title: "Stamps",
+	},
 ];
 
 // https://expressive-code.com/reference/configuration/
